c7969db14e6ffdcf743398c5a61d1a77b8a24604
nerik <erik.escoffier@gmail.com>
2016-02-26 14:21:48 +0100

    nearby

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var isoSQLTpl = cartodb._.template('SELECT * FROM nerikcarto.sxsw_iso WHERE name = \'<%= venueName %>\' AND type=\'<%= type %>\' ORDER BY data_range DESC');
 var venueSQLTpl = cartodb._.template($('#venueSQLTpl').html());
 var dayVenuesSQLTpl = cartodb._.template($('#dayVenuesSQLTpl').html());
+var nearbyVenuesSQLTpl = cartodb._.template($('#nearbyVenuesSQLTpl').html());
 
 var isoCssTpl = cartodb._.template($('#isoCssTpl').html());
 var venueTpl = cartodb._.template($('#venueTpl').html());
@@ -18,12 +19,32 @@ var gradient = new Color.Gradient([
     color: '#00B0D9'
   }
 ]);
+
+var isos = [1800,1200,900,600,300,120];
+
 var isoCss = isoCssTpl({
   gradient: gradient,
-  isos: [1800,1200,900,600,300,120]
+  isos: isos
 });
 
+var stops = {
+  120: {}
+}
+
 var audio = null;
+
+
+var currentVenue = {
+  name: 'BD Riley\'s',
+  ll: [30.4541,-97.7825],
+  id: 85,
+  isHotel: false
+}
+var currentMode = 'walk';
+var currentDay = 16;
+var venuesSublayer;
+var isoSublayer;
+var map;
 
 function main() {
   cartodb.createVis('map', 'https://team.cartodb.com/u/nerikcarto/api/v2/viz/18658374-d410-11e5-8f87-0e31c9be1b51/viz.json', {
@@ -36,13 +57,6 @@ function main() {
 }
 window.onload = main;
 
-var currentVenueName = 'BD Riley\'s';
-var currentVenueLL = [30.4541,-97.7825];
-var currentMode = 'walk';
-var currentDay = 16;
-var venuesSublayer;
-var isoSublayer;
-var map;
 
 var buildViz = function (vis, layers) {
   map = vis.getNativeMap();
@@ -61,8 +75,9 @@ var buildViz = function (vis, layers) {
 
   $('.js-controls input').on('click', function(e) {
     currentMode = $(e.target).val();
-    map.setView(currentVenueLL, (currentMode === 'car') ? 12 : 15);
-    loadIso(currentVenueName, currentMode);
+    map.setView(currentVenue.ll, (currentMode === 'car') ? 12 : 15);
+    loadIso(currentVenue.name, currentMode);
+    selectVenue(currentVenue.name, currentVenue.ll, currentVenue.isHotel, currentVenue.id);
   });
 
   $('.js-days button').on('click', function(e) {
@@ -89,26 +104,33 @@ var buildViz = function (vis, layers) {
   })
 
   selectDay(currentDay)
-  selectVenue(currentVenueName, currentVenueLL);
+  selectVenue(currentVenue.name, currentVenue.ll, false, currentVenue.id, true);
 }
 
 var onFeatureClick = function(e, latlng, pos, data, sublayerIndex) {
-  selectVenue(data.name, latlng, sublayerIndex === 1);
+  selectVenue(data.name, latlng, sublayerIndex === 1, data.cartodb_id);
 }
 
-var selectVenue = function(name, ll, isHotel) {
-  if (ll) currentVenueLL = ll;
-  currentVenueName = name;
-  if (isHotel) currentVenueName = 'hotel:' + currentVenueName;
-  loadIso(currentVenueName, currentMode);
-  loadVenueEvents(currentVenueName, currentDay, isHotel);
+var selectVenue = function(name, ll, isHotel, id, dontLoadEvents) {
+  currentVenue = {
+    name: name,
+    isHotel: isHotel,
+    id: id
+  }
+
+  if (ll) currentVenue.ll = ll;
+
+  if (isHotel) currentVenue.name = 'hotel:' + currentVenue.name;
+
+  loadIso(currentVenue.name, currentMode);
+  if (!dontLoadEvents) loadVenueEvents(currentVenue.name, currentDay, isHotel, currentVenue.id);
 }
 
 var selectDay = function(day) {
   currentDay = day;
   $('.js-days button').removeClass('selected');
   $('.js-days button[value='+day+']').addClass('selected');
-  loadVenueEvents(currentVenueName, currentDay);
+  loadVenueEvents(currentVenue.name, currentDay, currentVenue.isHotel, currentVenue.id);
   loadDayVenues(currentDay);
 }
 
@@ -125,8 +147,8 @@ var loadDayVenues = function (day) {
     day: day,
     allDays: day === 'allDays'
   });
-  console.log(sql)
-  venuesSublayer.setSQL(sql)
+  // console.log(sql)
+  venuesSublayer.setSQL(sql);
 }
 
 var dummyNearbyVenueTimes = [
@@ -159,14 +181,15 @@ var dummyNearbyVenueTimes = [
   }
 ];
 
-var loadVenueEvents = function (venueName, day, isHotel) {
+var loadVenueEvents = function (venueName, day, isHotel, venueId) {
+  var allDays = day === 'allDays';
   var sql = venueSQLTpl({
     venueName: venueName.replace('\'','\'\''),
     day: day,
-    allDays: day === 'allDays'
+    allDays: allDays
   });
 
-  console.log(sql)
+  // console.log(sql)
   var sqlClient = new cartodb.SQL({ user: 'nerikcarto' });
   sqlClient.execute(sql)
     .done(function(data) {
@@ -202,13 +225,61 @@ var loadVenueEvents = function (venueName, day, isHotel) {
       console.log('errors:' + errors);
     });
 
-  var nearbyVenuesHtml = nearbyVenuesTpl({
-    nearbyVenuesTimes: dummyNearbyVenueTimes,
-    modeLabel: (currentMode === 'walk') ? 'walking' : 'driving',
-    isHotel: isHotel,
+  var nearbySQL = nearbyVenuesSQLTpl({
+    center_id: venueId,
+    day: day,
+    allDays: allDays,
+    mode: currentMode
+  });
+  console.log(nearbySQL)
+  sqlClient.execute(nearbySQL)
+    .done(function(data) {
+      console.log(data);
+      var nearbyVenuesTimes = [];
 
-  })
-  $('.js-nearbyVenuesContainer').html(nearbyVenuesHtml);
+      function getRangeLabel(range) {
+        if (range === 120) {
+          return 'less than 2 minutes';
+        } else if (!range) {
+          return 'more than 30 minutes';
+        }
+        var prevIso = isos[isos.indexOf(range) + 1];
+        return prevIso/60 + ' to ' + range/60 + ' minutes'
+      }
+
+      data.rows.forEach(function (nearbyVenue) {
+        var range = nearbyVenue.data_range;
+        var nearbyVenueTime = _.findWhere(nearbyVenuesTimes, {range: range});
+        if (!nearbyVenueTime) {
+          nearbyVenueTime = {
+            range: range,
+            label: getRangeLabel(range),
+            color: (range) ? gradient.get((100/5)*isos.indexOf(range)).hex : gradient.get(0).hex,
+            nearbyVenues: []
+          }
+          nearbyVenuesTimes.push(nearbyVenueTime);
+        }
+
+        var eventsSummaryOverflow = nearbyVenue.events_summary.length > 40;
+        var eventsSummary = nearbyVenue.events_summary.substr(0, 40);
+        if (eventsSummaryOverflow) eventsSummary += '(…)'
+
+        nearbyVenueTime.nearbyVenues.push({
+          name: nearbyVenue.name,
+          events: eventsSummary
+        })
+      })
+
+      var nearbyVenuesHtml = nearbyVenuesTpl({
+        nearbyVenuesTimes: nearbyVenuesTimes,
+        modeLabel: (currentMode === 'walk') ? 'walking' : 'driving',
+        isHotel: isHotel,
+
+      })
+      $('.js-nearbyVenuesContainer').html(nearbyVenuesHtml);
+    });
+
+
 
 }
 
